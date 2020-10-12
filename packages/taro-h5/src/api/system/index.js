5cb52745ccc4839c3dda09652285789638b7538f
黎贵奇 <carney520@hotmail.com>
2020-10-12 15:44:16 +0800

    fix(h5): 公众号一维码扫码结果错误

--- packages/taro-h5/src/api/system/index.js
+++ packages/taro-h5/src/api/system/index.js
@@ -2,8 +2,21 @@ import { getSystemInfo, getSystemInfoSync } from './info'
 import { getNetworkType, onNetworkStatusChange } from './network'
 import { processOpenapi } from '../utils'
 
-export const scanCode = processOpenapi('scanQRCode', { needResult: 1 }, res => ({
-  errMsg: res.errMsg === 'scanQRCode:ok' ? 'scanCode:ok' : res.errMsg,
-  result: res.resultStr
-}))
+export const scanCode = processOpenapi('scanQRCode', { needResult: 1 }, res => {
+  const raw = res.resultStr
+  let result = raw
+  let scanType = 'QR_CODE'
+  let commaIdx = result.indexOf(',')
+  if (commaIdx !== -1) {
+    scanType = result.slice(0, commaIdx)
+    result = result.slice(commaIdx + 1)
+  }
+
+  return ({
+    errMsg: res.errMsg === 'scanQRCode:ok' ? 'scanCode:ok' : res.errMsg,
+    result: result,
+    scanType,
+    raw
+  })
+})
 export { getSystemInfo, getSystemInfoSync, getNetworkType, onNetworkStatusChange }
